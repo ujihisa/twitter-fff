2f975236b1240f8f84f35b304f5d9369c70babe1
ujihisa <ujihisa@gmail.com>
2009-01-07 16:12:26 +0900

    add #write_candidates() * use my forked twitter library instead of gem twitter * remove #follow_candidates() * and add the supporting method named #write_candidates()

--- lib/fff.rb
+++ lib/fff.rb
@@ -1,11 +1,11 @@
 require 'yaml'
-require 'rubygems'
-require 'twitter'
+require '~/git/twitter/lib/twitter'
 
 class Fff
   def initialize(u, p)
     @t = Twitter::Base.new(u, p)
-    @your_followings = @t.friends.map(&:screen_name)
+    # NOTE: Twitter::Base#all_friends is not officialy distributed in the gem twitter library.
+    @your_followings = @t.all_friends.map(&:screen_name)
   end
 
   # followings :: ScreenName -> [ScreenName]
@@ -18,11 +18,20 @@ class Fff
     @your_followings.include? s
   end
 
-  # follow_candidates :: [ScreenName]
-  def follow_candidates
-    @your_followings.inject([]) {|memo, s|
-      memo += followings(s)
-      memo.uniq
-    } - @your_followings
+  # follow_candidates :: String -> IO ()
+  def write_candidates(filename)
+    File.open(filename, 'w') do |io|
+      @your_followings.each do |s|
+        begin
+          ff = followings(s) - @your_followings
+          io.puts "#{s}: #{ff.inspect}"
+          sleep 1
+        rescue Twitter::CantConnect => e
+          p e
+          io.puts "# #{s}: skipped."
+          sleep 60
+        end
+      end
+    end
   end
 end
